562e80001b50a93154c6db6d7f34364e6d636558
Coleman <jackson@gariety.xxx>
2017-01-03 18:05:50 -0800

    Runners up.

--- app/models/competition.go
+++ app/models/competition.go
@@ -28,8 +28,30 @@ func (competition *Competition) Previous() (*Competition, error) {
 	c := DB.Order("date asc").Where("date = ?", utils.LastSaturday()).First(competition)
 	return competition, c.Error
 }
+
 func (competition Competition) IsCurrent() bool {
 	return competition.Date.Equal(utils.NextSaturday())
+}
+
+func (competition Competition) IsPast() bool {
+	return competition.Date.Before(utils.NextSaturday())
+}
+
+func (competition Competition) RunnersUp() Entries {
+	runnersUp := Entries{}
+	winner := competition.Winner()
+	DB.Model(&competition).Related(&runnersUp)
+
+// Remove the winner
+	for i, entry := range runnersUp {
+		if entry.ID == winner.ID {
+			runnersUp = append(runnersUp[:i], runnersUp[i+1:]...)
+		} else {
+			DB.Model(&entry).Related(&runnersUp[i].User)
+		}
+	}
+
+	return runnersUp
 }
 
 func (competition Competition) AverageExecTime() float64 {
