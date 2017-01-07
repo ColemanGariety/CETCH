74670b7bf52ca807f802c16fb879e8c509889c93
Coleman <jackson@gariety.xxx>
2017-01-07 11:32:05 -0800

    Fix runners up

--- app/models/competition.go
+++ app/models/competition.go
@@ -37,20 +37,15 @@ func (competition Competition) IsPast() bool {
 	return competition.Date.Before(utils.NextSaturday())
 }
 
-func (competition Competition) RunnersUp() Entries {
-	runnersUp := Entries{}
-	winner := competition.Winner()
-	DB.Model(&competition).Related(&runnersUp)
-	// Remove the winner
-	for i, entry := range runnersUp {
-		if entry.ID == winner.ID {
-			runnersUp = append(runnersUp[:i], runnersUp[i+1:]...)
-		} else {
-			DB.Model(&entry).Related(&runnersUp[i].User)
-		}
+func (competition Competition) All() Entries {
+	all := Entries{}
+	DB.Order("exec_time asc").Model(&competition).Related(&all)
+
+	for i, entry := range all {
+		DB.Model(&entry).Related(&all[i].User)
 	}
 
-	return runnersUp
+	return all
 }
 
 func (competition Competition) AverageExecTime() float64 {
